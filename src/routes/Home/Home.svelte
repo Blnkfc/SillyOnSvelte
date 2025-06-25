958fa52e7a2ac865c4386c710311c22d29ae0e2e
Blnkfc <pavlinus11v@gmail.com>
2025-06-25 23:37:54 +0300

    Started on some serious stuff

---
+++ src/routes/Home/Home.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { API_STRING } from "../../../constants";
+  import axios from "axios";
+
+  let currentPage = $state(1);
+  let data = $state<any>([]);
+
+  $effect(() => {
+    const result = axios.get(API_STRING + `artworks?page=${currentPage}&limit=10&fields=title`).then((res) => {
+        data = res.data.data
+        console.log('data', data);
+        
+    });
+  });
+</script>
+
+<div>
+    {#each data as item}
+        <div>{item.title}</div>
+    {/each}
+
+</div>
